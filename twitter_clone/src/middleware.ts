4eb0895efc9f38c0e1e64168f7a8a213ebbaf4ea
Ng Jun Long <42053960+ngjunlong9651@users.noreply.github.com>
2023-04-26 15:29:10 +0000

    Added time since post

---
+++ twitter_clone/src/middleware.ts
@@ -0,0 +1,11 @@
+import { withClerkMiddleware, getAuth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export default withClerkMiddleware((request: NextRequest) => {
+  return NextResponse.next();
+});
+
+export const config = {
+  matcher: "/((?!_next/image|_next/static|favicon.ico).*)",
+};
